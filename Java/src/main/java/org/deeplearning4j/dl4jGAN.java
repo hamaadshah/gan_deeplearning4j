c90213a343f1f0ca76c09f58e1165b3339d5add7
hamaadshah <hamaadshah@hotmail.com>
2018-12-29 00:43:17 +0000

    Almost there with this GAN business.

--- Java/src/main/java/org/deeplearning4j/dl4jGAN.java
+++ Java/src/main/java/org/deeplearning4j/dl4jGAN.java
@@ -39,7 +39,7 @@ import org.slf4j.LoggerFactory;
 public class dl4jGAN {
     private static final Logger log = LoggerFactory.getLogger(dl4jGAN.class);
 
-    private static final int batchSizePerWorker = 5000;
+    private static final int batchSizePerWorker = 10000;
     private static final int batchSizePred = 1;
     private static final int numEpochs = 1;
     private static final int numLinesToSkip = 0;
@@ -52,22 +52,22 @@ public class dl4jGAN {
 
     private static final String delimiter = ",";
 
+    private static final boolean useGpu = false;
+
     public static void main(String[] args) throws Exception {
         new dl4jGAN().GAN(args);
     }
 
     private void GAN(String[] args) throws Exception {
-        /*
-        Nd4j.setDataType(DataBuffer.Type.FLOAT);
+        if (useGpu) {
+            Nd4j.setDataType(DataBuffer.Type.FLOAT);
 
-        CudaEnvironment.getInstance().getConfiguration()
-                .allowMultiGPU(true)
-                .setMaximumDeviceCache(4L * 2L * 1024L * 1024L * 1024L)
-                .allowCrossDeviceAccess(true)
-                .setVerbose(true);
-        System.out.println(CudaEnvironment.getInstance().getConfiguration());
-        System.out.println(CudaEnvironment.getInstance().getCurrentDeviceArchitecture());
-        */
+            CudaEnvironment.getInstance().getConfiguration()
+                    .allowMultiGPU(true)
+                    .setMaximumDeviceCache(2L * 1024L * 1024L * 1024L)
+                    .allowCrossDeviceAccess(true)
+                    .setVerbose(true);
+        }
 
         Nd4j.getRandom().setSeed(666);
 
@@ -300,32 +300,68 @@ public class dl4jGAN {
                     trainDataList.add(new DataSet(gen.output(Nd4j.randn(1, zSize))[0], Nd4j.hstack(Nd4j.ones(1, 1), Nd4j.zeros(1, 1))));
                 }
                 JavaRDD<DataSet> trainData = sc.parallelize(trainDataList);
+                log.info("Training discriminator!");
                 sparkDis.fit(trainData);
 
                 // Update frozen discriminator with discriminator.
-                gan.getLayer("gan_dis_output_layer_15").setParam("gamma", dis.getLayer("dis_batch_layer_1").getParam("gamma"));
-                gan.getLayer("gan_dis_output_layer_15").setParam("beta", dis.getLayer("dis_batch_layer_1").getParam("beta"));
-                gan.getLayer("gan_dis_output_layer_15").setParam("mean", dis.getLayer("dis_batch_layer_1").getParam("mean"));
-                gan.getLayer("gan_dis_output_layer_15").setParam("var", dis.getLayer("dis_batch_layer_1").getParam("var"));
+                gan.getLayer("gan_dis_batch_layer_10").setParam("gamma", dis.getLayer("dis_batch_layer_1").getParam("gamma"));
+                gan.getLayer("gan_dis_batch_layer_10").setParam("beta", dis.getLayer("dis_batch_layer_1").getParam("beta"));
+                gan.getLayer("gan_dis_batch_layer_10").setParam("mean", dis.getLayer("dis_batch_layer_1").getParam("mean"));
+                gan.getLayer("gan_dis_batch_layer_10").setParam("var", dis.getLayer("dis_batch_layer_1").getParam("var"));
 
-                gan.getLayer("gan_dis_conv2d_layer_10").setParam("W", dis.getLayer("dis_conv2d_layer_2").getParam("W"));
-                gan.getLayer("gan_dis_conv2d_layer_10").setParam("b", dis.getLayer("dis_conv2d_layer_2").getParam("b"));
+                gan.getLayer("gan_dis_conv2d_layer_11").setParam("W", dis.getLayer("dis_conv2d_layer_2").getParam("W"));
+                gan.getLayer("gan_dis_conv2d_layer_11").setParam("b", dis.getLayer("dis_conv2d_layer_2").getParam("b"));
 
-                gan.getLayer("gan_dis_conv2d_layer_12").setParam("W", dis.getLayer("dis_conv2d_layer_4").getParam("W"));
-                gan.getLayer("gan_dis_conv2d_layer_12").setParam("b", dis.getLayer("dis_conv2d_layer_4").getParam("b"));
+                gan.getLayer("gan_dis_conv2d_layer_13").setParam("W", dis.getLayer("dis_conv2d_layer_4").getParam("W"));
+                gan.getLayer("gan_dis_conv2d_layer_13").setParam("b", dis.getLayer("dis_conv2d_layer_4").getParam("b"));
 
-                gan.getLayer("gan_dis_dense_layer_14").setParam("W", dis.getLayer("dis_dense_layer_6").getParam("W"));
-                gan.getLayer("gan_dis_dense_layer_14").setParam("b", dis.getLayer("dis_dense_layer_6").getParam("b"));
+                gan.getLayer("gan_dis_dense_layer_15").setParam("W", dis.getLayer("dis_dense_layer_6").getParam("W"));
+                gan.getLayer("gan_dis_dense_layer_15").setParam("b", dis.getLayer("dis_dense_layer_6").getParam("b"));
+
+                gan.getLayer("gan_dis_output_layer_16").setParam("W", dis.getLayer("dis_output_layer_7").getParam("W"));
+                gan.getLayer("gan_dis_output_layer_16").setParam("b", dis.getLayer("dis_output_layer_7").getParam("b"));
 
                 trainDataList = new ArrayList<>();
                 for (int i = 0; i < batchSizePerWorker; i++) {
-                    // [Real, Real].
-                    trainDataList.add(new DataSet(gen.output(Nd4j.randn(1, zSize))[0], Nd4j.hstack(Nd4j.zeros(1, 1), Nd4j.ones(1, 1))));
+                    // Tell the frozen discriminator that all the fake examples are real examples.
+                    // [Fake, Real].
+                    trainDataList.add(new DataSet(Nd4j.randn(1, zSize), Nd4j.hstack(Nd4j.zeros(1, 1), Nd4j.ones(1, 1))));
                 }
                 trainData = sc.parallelize(trainDataList);
+                log.info("Training generator!");
                 sparkGan.fit(trainData);
 
                 // Update generator with GAN's generator.
+                gen.getLayer("gen_batch_1").setParam("gamma", gan.getLayer("gan_batch_1").getParam("gamma"));
+                gen.getLayer("gen_batch_1").setParam("beta", gan.getLayer("gan_batch_1").getParam("beta"));
+                gen.getLayer("gen_batch_1").setParam("mean", gan.getLayer("gan_batch_1").getParam("mean"));
+                gen.getLayer("gen_batch_1").setParam("var", gan.getLayer("gan_batch_1").getParam("var"));
+
+                gen.getLayer("gen_dense_layer_2").setParam("W", gan.getLayer("gan_dense_layer_2").getParam("W"));
+                gen.getLayer("gen_dense_layer_2").setParam("b", gan.getLayer("gan_dense_layer_2").getParam("b"));
+
+                gen.getLayer("gen_dense_layer_3").setParam("W", gan.getLayer("gan_dense_layer_3").getParam("W"));
+                gen.getLayer("gen_dense_layer_3").setParam("b", gan.getLayer("gan_dense_layer_3").getParam("b"));
+
+                gen.getLayer("gen_batch_4").setParam("gamma", gan.getLayer("gan_batch_4").getParam("gamma"));
+                gen.getLayer("gen_batch_4").setParam("beta", gan.getLayer("gan_batch_4").getParam("beta"));
+                gen.getLayer("gen_batch_4").setParam("mean", gan.getLayer("gan_batch_4").getParam("mean"));
+                gen.getLayer("gen_batch_4").setParam("var", gan.getLayer("gan_batch_4").getParam("var"));
+
+                gen.getLayer("gen_deconv2d_5").setParam("W", gan.getLayer("gan_deconv2d_5").getParam("W"));
+                gen.getLayer("gen_deconv2d_5").setParam("b", gan.getLayer("gan_deconv2d_5").getParam("b"));
+
+                gen.getLayer("gen_conv2d_6").setParam("W", gan.getLayer("gan_conv2d_6").getParam("W"));
+                gen.getLayer("gen_conv2d_6").setParam("b", gan.getLayer("gan_conv2d_6").getParam("b"));
+
+                gen.getLayer("gen_deconv2d_7").setParam("W", gan.getLayer("gan_deconv2d_7").getParam("W"));
+                gen.getLayer("gen_deconv2d_7").setParam("b", gan.getLayer("gan_deconv2d_7").getParam("b"));
+
+                gen.getLayer("gen_conv2d_8").setParam("W", gan.getLayer("gan_conv2d_8").getParam("W"));
+                gen.getLayer("gen_conv2d_8").setParam("b", gan.getLayer("gan_conv2d_8").getParam("b"));
+
+                gen.getLayer("gen_output_layer_9").setParam("W", gan.getLayer("gan_dense_layer_9").getParam("W"));
+                gen.getLayer("gen_output_layer_9").setParam("b", gan.getLayer("gan_dense_layer_9").getParam("b"));
 
                 log.info("Completed Batch {}!", batch_counter + 1);
                 batch_counter++;
@@ -343,13 +379,9 @@ public class dl4jGAN {
             testDataList.add(iterTest.next());
         }
 
-        for (int i = 0; i < testDataList.size(); i++) {
-            testDataList.add(new DataSet(gen.output(Nd4j.randn(1, zSize))[0], Nd4j.hstack(Nd4j.ones(1,1), Nd4j.zeros(1,1))));
-        }
-
         JavaRDD<DataSet> testData = sc.parallelize(testDataList);
 
-        log.info("Number of testing examples: {}!", testDataList.iterator());
+        log.info("Number of testing examples: {}!", testDataList.size());
 
         Evaluation evaluation = sparkDis.doEvaluation(testData, batchSizePred, new Evaluation(numClasses))[0];
         log.info(evaluation.stats());
